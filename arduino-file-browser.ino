35ece3aa0885f2a7cbf8b9be7333a2b604cd1f7d
Matt Kendrick <68827500+matt-kendrick@users.noreply.github.com>
2021-02-21 11:40:17 -0500

    added file count

--- arduino-file-browser.ino
+++ arduino-file-browser.ino
@@ -11,26 +11,82 @@ int sdChipSelectPin = 4;
 
 U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(screenClockPin, screenDataPin);
 
+File root;
+
+int fileCount, dirCount;
+
 void setup() {
   //setup screen
   u8x8.begin();
   u8x8.clear();
   u8x8.setCursor(0,0);
   u8x8.setFont(u8x8_font_chroma48medium8_r); 
-
   u8x8.print("Screen Ready.");
   u8x8.setCursor(0,1);
 
-  if (!SD.begin(4)) {
+  if (!SD.begin(sdChipSelectPin)) {
     u8x8.print("SD Init Fail!");
     while (1);
-  } else {
-    u8x8.print("SD Card Ready.");
   }
 
+  u8x8.print("SD Card Ready.");
+
   u8x8.setCursor(0,2);
+  root = SD.open("/");
+  setFileCounts(root, dirCount, fileCount);
+
+  String countLabel = "File Count: ";
+  countLabel.concat(fileCount);
+  u8x8.print(countLabel);
+  
+  delay(500);
+
+  u8x8.clear();
+  u8x8.setCursor(0,0);
+
+  u8x8.print(getFileName(root, 1));
 }
 
 void loop() {
   
+}
+
+void setFileCounts(File dir, int & dirCount, int & fileCount) {
+  dirCount = 0;
+  fileCount = 0;
+  
+  while(true) {
+    File entry = dir.openNextFile();
+    if(!entry) {
+      dir.rewindDirectory();
+      break;
+    } else {
+      if(!entry.isDirectory()) {
+        fileCount++;
+      } else {
+        dirCount++;
+      }
+    }
+  }
+}
+
+char * getFileName(File dir, int fileNumber) {
+  int fileCount = 0;
+  
+  while(true) {
+    File entry = dir.openNextFile();
+    
+    if(!entry) {
+      dir.rewindDirectory();
+    } else {
+      if(!entry.isDirectory()){
+        fileCount++;
+  
+        if(fileCount == fileNumber) {
+          dir.rewindDirectory();
+          return entry.name();
+        }
+      }
+    }
+  }
 }
